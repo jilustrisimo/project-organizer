aa0c5642c196cbe358c9192315b9ed6fe69a6821
github username <github email address>
2017-02-12 21:30:07 -0500

    added validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :projects
   has_secure_password
+
+  validates :username, presense: true, uniqueness: true
+  validates :password, presense: true, length: { minimum: 8 }
 end
