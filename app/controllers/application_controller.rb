f91f1baf82ece4c21baeb26bb1b7a5cf81b1cd16
github username <github email address>
2017-02-12 21:44:46 -0500

    built application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, '%B1F2Oi*aj%ZD69#sbpxZ48%MQDbIo'
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :projects
   has_secure_password
 
-  validates :username, presense: true, uniqueness: true
-  validates :password, presense: true, length: { minimum: 8 }
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
