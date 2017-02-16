598e8b9c45d5cd47818587b1980d09711a407cb0
github username <github email address>
2017-02-15 22:58:25 -0500

    added completed column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215231231) do
+ActiveRecord::Schema.define(version: 20170216035713) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170215231231) do
     t.datetime "due_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "completed"
   end
 
   create_table "tasks", force: :cascade do |t|
