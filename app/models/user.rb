ff51aa021f1d0fc3c76435e78b2654dc90d6974d
github username <jilustrisimo@users.noreply.github.com>
2017-09-12 13:19:11 -0400

    testing pg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :projects, dependent: :null
+  has_many :projects, dependent: :nullify
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
